104dc489ca7518604de5d681de4e79ee7a44075c
gc-plp <plp.github@gmail.com>
2018-12-15 12:16:54 +0200

    Start adding trigger logic

--- main.py
+++ main.py
@@ -2,6 +2,14 @@ from spanky.bot import Bot
 
 bot = Bot()
 
+class dummy_msg():
+    pass
+
+msg = dummy_msg()
+msg.text = ".system"
+
+bot.on_message(msg)
+
 while True:
     import time
     time.sleep(1)
--- spanky/bot.py
+++ spanky/bot.py
@@ -1,16 +1,27 @@
 import json
 
 from spanky.plugin.plugin_manager import PluginManager
+from spanky.plugin.event import EventContainer, EventType
 from spanky.database.db import db_data
 
 class Bot():
     def __init__(self):
         with open('bot_config.json') as data_file:
             self.config = json.load(data_file)
-            
+
         db_path = self.config.get('database', 'sqlite:///cloudbot.db')
         # Open the database first
         self.db = db_data(db_path)
-        
+
         self.plugin_manager = PluginManager(
             self.config.get("plugin_paths", ""), self.db)
+
+    def on_message_edit(self, before, after):
+        pass
+
+    def on_message(self, message):
+        return EventContainer(
+            EventType.message,
+            {
+                "message": message
+            })
