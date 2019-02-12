a353beae1d87d4b1e07d0813016fc50338fd9cca
gc-plp <gabrielcristian.ionescu@gmail.com>
2019-02-12 12:44:08 +0000

    Add qalc interface

---
+++ plugins/qalc.py
@@ -0,0 +1,8 @@
+import subprocess
+from spanky.plugin import hook
+
+@hook.command()
+def calc(text):
+    """<expression> - qalc interface"""
+    proc = subprocess.run(["qalc", text], timeout=1, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    return "`%s`" % proc.stdout.decode()
