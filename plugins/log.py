8edf4a9979e0285d637295b65da3f1a799bab156
gc-plp <gabrielcristian.ionescu@gmail.com>
2019-03-17 12:33:20 +0000

    Add db logger

--- plugins/log.py
+++ plugins/log.py
@@ -1,6 +1,7 @@
 import os
 import codecs
 import time
+import psycopg2
 
 from spanky.plugin import hook
 from spanky.plugin.event import EventType
@@ -9,25 +10,40 @@ base_formats = {
     EventType.message: "{msg_id}: [{hour}:{minute}:{second}] <{nick}> {content}",
 }
 
+db_conn = None
+
+@hook.on_start()
+def init_db(bot):
+    global db_conn
+
+    db_name = bot.config.get("db_name", None)
+    db_user = bot.config.get("db_user", None)
+
+    if db_name != None and db_user != None:
+        db_conn = psycopg2.connect("dbname=%s user=%s" % (db_name, db_user))
+
 def get_format_args(event):
     # Setup arguments
     hour, minute, second = time.strftime("%H,%M,%S").split(',')
 
     args = {
         "msg_id": event.msg.id,
-        "server": event.server.name, 
+        "server": event.server.name,
         "server_id": event.server.id,
-        "channel": event.channel.name, 
+        "channel": event.channel.name,
         "nick": event.author.name + "/" + event.author.nick + "/" + event.author.id,
+        "author": event.author.name,
+        "author_id": event.author.id,
         "hour": hour,
         "minute": minute,
-        "second": second
+        "second": second,
+        "timestamp": time.strftime("%Y-%m-%d %H:%M:%S")
     }
 
     if event.text is not None:
         args["content"] = event.text
     else:
-        args["content"] = None
+        args["content"] = ""
 
     return args
 
@@ -48,10 +64,10 @@ def get_log_filename(event, args):
     current_time = time.localtime()
     folder_name = time.strftime(folder_format, current_time)
     file_name = time.strftime(file_format, current_time).lower()
-    
+
     folder_name = folder_name.format(**args)
     file_name = file_name.format(**args)
-    
+
     return os.path.join("logs", folder_name, file_name)
 
 
@@ -79,8 +95,14 @@ def get_log_stream(event, args):
     return log_stream
 
 @hook.event(EventType.message)
-def log(event):
+def log(bot, event):
     args = get_format_args(event)
+
+    file_log(event, args)
+    console_log(bot, event, args)
+    db_log(event, args)
+
+def file_log(event, args):
     text = format_event(event, args)
 
     if text is not None:
@@ -88,11 +110,17 @@ def log(event):
         stream.write(text + os.linesep)
         stream.flush()
 
-
-@hook.event(EventType.message)
-def console_log(bot, event):
-    args = get_format_args(event)
+def console_log(bot, event, args):
     text = format_event(event, args)
-    
+
     if text is not None:
         bot.logger.info(text)
+
+def db_log(event, args):
+    if not db_conn:
+        return
+
+    cs = db_conn.cursor()
+    cs.execute("""insert into messages (id, date, author, author_id, msg, channel, server, server_id) \
+            values({msg_id}, \'{timestamp}\', \'{author}\', {author_id}, \'{content}\', \'{channel}\', \'{server}\', {server_id})""".format(**args))
+    db_conn.commit()
