e3a547b897b4be07fb62134d6c9bddea96092abd
gc-plp <plp.github@gmail.com>
2019-04-20 11:21:45 +0000

    Add unused commands

---
+++ plugins/disabled/others.py
@@ -0,0 +1,36 @@
+from spanky.plugin import hook
+
+@hook.command()
+async def do_assign(bot):
+    try:
+        import discord
+        client = bot.backend.client
+
+        ro = None
+        for s in client.servers:
+            if s.name == "r/Romania":
+                ro = s
+                break
+
+        role = None
+        for r in ro.roles:
+            if r.name == "Șef de scară":
+                role = r
+                break
+
+        for m in ro.members:
+            has = False
+            for urole in m.roles:
+                if urole.name == role.name:
+                    has = True
+                    break
+
+            if has == False:
+                print(m)
+                await client.add_roles(m, role)
+                import time
+                time.sleep(0.2)
+
+    except:
+        import traceback
+        traceback.print_exc()
