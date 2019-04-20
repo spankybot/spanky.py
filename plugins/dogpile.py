6de98c09c3ed5a5ab97d9ac7b12ded3e6e7276c5
gc-plp <plp.github@gmail.com>
2019-04-20 11:07:07 +0000

    Remove verification and fix formatting

--- plugins/dogpile.py
+++ plugins/dogpile.py
@@ -7,7 +7,7 @@ from bs4 import BeautifulSoup
 
 from spanky.plugin import hook
 
-search_url = "http://dogpile.com/search"
+search_url = "http://www.dogpile.com/search"
 
 HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Linux; Android 4.0.4; Galaxy Nexus Build/IMM76B) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.133 Mobile Safari/535.19'
@@ -19,9 +19,10 @@ def dogpileimage(text):
     """<query> - Uses the dogpile search engine to search for images."""
     image_url = search_url + "/images"
     params = {'q': " ".join(text.split())}
-    r = requests.get(image_url, params=params, headers=HEADERS)
+    r = requests.get(image_url, params=params, headers=HEADERS, verify=False)
     r.raise_for_status()
     soup = BeautifulSoup(r.content)
+    print(soup)
     data = soup.find_all("script")[6].string
     link_re = re.compile('"url":"(.*?)",')
     linklist = link_re.findall(data)
@@ -37,7 +38,7 @@ def dogpile(text):
     """<query> - Uses the dogpile search engine to find shit on the web."""
     web_url = search_url + "/web"
     params = {'q': " ".join(text.split())}
-    r = requests.get(web_url, params=params, headers=HEADERS)
+    r = requests.get(web_url, params=params, headers=HEADERS, verify=False)
     r.raise_for_status()
     soup = BeautifulSoup(r.content)
     results = soup.find('div', id="webResults")
@@ -48,4 +49,4 @@ def dogpile(text):
         parse.unquote(results.find_all('a', {'class': 'resultDisplayUrl'})[0]['href']).split(
             'ru=')[1].split('&')[0])
     result_description = results.find_all('div', {'class': 'resultDescription'})[0].text
-    return "{} -- \x02{}\x02".format(result_url, result_description)
+    return "{} -- {}".format(result_url, result_description)
