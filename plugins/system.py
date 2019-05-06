54fcf438c56645eb069af5eef2effad09f4f6cf9
gc-plp <plp.github@gmail.com>
2019-05-06 23:17:03 +0000

    Add memory tracer

--- plugins/system.py
+++ plugins/system.py
@@ -2,14 +2,63 @@ import platform
 import psutil
 import os
 import time
+import tracemalloc
+import linecache
 from datetime import timedelta
 from spanky.utils.filesize import size as format_bytes
+from spanky.plugin.permissions import Permission
 from spanky.plugin import hook
+
+start_trace = None
 
 @hook.command()
 def about():
-    return "Bot source code can be found at https://github.com/gc-plp/spanky.py or ask the owner: plp#7552"
+    return "Bot source code can be found at https://github.com/gc-plp/spanky.py or ask the owner: plp#9999"
 
+@hook.command(permissions=Permission.admin)
+def start_tracemalloc():
+    global start_trace
+
+    tracemalloc.start(100)
+    start_trace = tracemalloc.take_snapshot()
+
+@hook.command(permissions=Permission.admin)
+def stop_tracemalloc():
+    tracemalloc.stop()
+
+@hook.command(permissions=Permission.admin)
+def mem_snapshot():
+    key_type='lineno'
+    limit=10
+
+    rval = '#\n'
+    snapshot = tracemalloc.take_snapshot()
+    top_stats = snapshot.compare_to(start_trace, key_type)
+
+    # snapshot = snapshot.filter_traces((
+    #     tracemalloc.Filter(False, "<frozen importlib._bootstrap>"),
+    #     tracemalloc.Filter(False, "<unknown>"),
+    # ))
+
+    rval += "Top %s lines\n" % limit
+    for index, stat in enumerate(top_stats[:limit], 1):
+        frame = stat.traceback[0]
+        # replace "/path/to/module/file.py" with "module/file.py"
+        filename = os.sep.join(frame.filename.split(os.sep)[-2:])
+        rval += "#%s: %s:%s: %.1f KiB\n" % (index, filename, frame.lineno, stat.size / 1024)
+
+        line = linecache.getline(frame.filename, frame.lineno).strip()
+        if line:
+            rval += '    %s\n' % line
+
+    other = top_stats[limit:]
+    if other:
+        size = sum(stat.size for stat in other)
+        rval += "%s other: %.1f KiB\n" % (len(other), size / 1024)
+    total = sum(stat.size for stat in top_stats)
+    rval += "Total allocated size: %.1f KiB\n" % (total / 1024)
+
+    return rval
 
 @hook.command()
 def system(send_message):
