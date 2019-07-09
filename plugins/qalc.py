6c6bbc14169d8168bfa148168b620a51f8d88edb
gc-plp <plp.github@gmail.com>
2019-07-09 11:17:06 +0000

    Increase timeout to 3s

--- plugins/qalc.py
+++ plugins/qalc.py
@@ -4,5 +4,5 @@ from spanky.plugin import hook
 @hook.command()
 def calc(text):
     """<expression> - qalc interface"""
-    proc = subprocess.run(["qalc", text], timeout=1, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    proc = subprocess.run(["qalc", text], timeout=3, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     return "`%s`" % proc.stdout.decode()
