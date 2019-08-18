53c92138d13f9e48e9394a5fdb26bcf0a40820f7
gc-plp <plp.github@gmail.com>
2019-08-18 17:58:12 +0300

    Add Docker container to run the bot

---
+++ Dockerfile/files/startup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [[ ! -d /botsrc/.pyenv ]]; then
+    git clone https://github.com/pyenv/pyenv.git /botsrc/.pyenv
+fi
+
+echo 'export PYENV_ROOT="/botsrc/.pyenv"' >> ~/.bashrc
+echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(pyenv init -)"' >> ~/.bashrc
+source ~/.bashrc
+
+pyenv install -s 3.5.7
+pyenv shell 3.5.7
+
+cd /botsrc/
+pip3 install -r requirements.txt
+
+while true; do python3.5 main.py && sleep 1; done
