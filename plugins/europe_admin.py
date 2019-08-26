fafbcdd8703c7aad39f1351fb020f2de7054c591
gc-plp <plp.github@gmail.com>
2019-08-26 20:05:46 +0000

    Send embeds on /r/europe

--- plugins/europe_admin.py
+++ plugins/europe_admin.py
@@ -21,7 +21,7 @@ spam_check = {}
 SPAM_LIMIT = 4
 
 @hook.event(EventType.message, server_id=EUROPE_ID)
-def check_spam(bot, event, send_pm, str_to_id, send_message):
+def check_spam(bot, event, send_pm, str_to_id, send_message, send_embed):
     if event.author.bot:
         return
 
@@ -57,12 +57,12 @@ def check_spam(bot, event, send_pm, str_to_id, send_message):
                 for k, v in reason.items():
                     mod_action_text += "**%s:** %s\n" % (k, v)
 
-                send_message(text=mod_action_text, target="#mod-actions")
+                send_embed("User confined for spam", "", {"Details": mod_action_text}, target="#mod-actions")
 
             send_message(ret)
 
 @hook.command(permissions=Permission.admin, server_id=EUROPE_ID)
-def gulag(send_message, text, server, event, bot, str_to_id):
+def gulag(send_message, text, server, event, bot, str_to_id, send_embed):
     """<user, duration> - assign gulag role for specified time - duration can be seconds, minutes, hours, days.\
  To set a 10 minute 15 seconds timeout for someone, type: '.gulag @user 10m15s'.\
  The abbrebiations are: s - seconds, m - minutes, h - hours, d - days.
@@ -70,11 +70,12 @@ def gulag(send_message, text, server, event, bot, str_to_id):
     ret, reason = assign_temp_role(rstorage, roddit, bot, "Gulag", text, "gulag", str_to_id, event)
 
     if reason:
-       gulag_text = "-\n"
+       gulag_text = ""
        for k, v in reason.items():
            gulag_text += "**%s:** %s\n" % (k, v)
 
-       send_message(text=gulag_text, target="#mod-actions")
+       #send_message(text=gulag_text, target="#mod-actions")
+       send_embed("User gulaged", "", {"Details": gulag_text}, target="#mod-actions")
     send_message(ret)
 
 @hook.on_ready(server_id=EUROPE_ID)
@@ -110,7 +111,7 @@ async def userhistory(text, str_to_id, storage, async_send_message):
         traceback.print_exc()
 
 @hook.command(permissions=Permission.admin, server_id=EUROPE_ID)
-def confine(send_message, text, server, event, bot, str_to_id):
+def confine(send_message, text, server, event, bot, str_to_id, send_embed):
     """<user, duration> - assign confinment role for specified time - duration can be seconds, minutes, hours, days.\
  To set a 10 minute 15 seconds timeout for someone, type: '.confine @user 10m15s'.\
  The abbrebiations are: s - seconds, m - minutes, h - hours, d - days.
@@ -118,11 +119,27 @@ def confine(send_message, text, server, event, bot, str_to_id):
     ret, reason = assign_temp_role(rstorage, roddit, bot, "Temporary Confinement", text, "confine", str_to_id, event)
 
     if reason:
+        gulag_text = ""
+        for k, v in reason.items():
+            gulag_text += "**%s:** %s\n" % (k, v)
+
+        send_embed("User confined", "", {"Details": gulag_text}, target="#mod-actions")
+    send_message(ret)
+
+@hook.command(permissions=Permission.admin, server_id=EUROPE_ID)
+def furrygulag(send_message, text, server, event, bot, str_to_id, send_embed):
+    """<user, duration> - assign Furry Inmate role for specified time - duration can be seconds, minutes, hours, days.\
+ To set a 10 minute 15 seconds timeout for someone, type: '.furrygulag @user 10m15s'.\
+ The abbrebiations are: s - seconds, m - minutes, h - hours, d - days.
+    """
+    ret, reason = assign_temp_role(rstorage, roddit, bot, "Furry Inmate", text, "furrygulag", str_to_id, event)
+
+    if reason:
         gulag_text = "-\n"
         for k, v in reason.items():
             gulag_text += "**%s:** %s\n" % (k, v)
 
-        send_message(text=gulag_text, target="#mod-actions")
+        send_embed("User furrygulaged", "", {"Details": gulag_text}, target="#mod-actions")
     send_message(ret)
 
 @hook.command(permissions=Permission.admin, server_id=EUROPE_ID)
