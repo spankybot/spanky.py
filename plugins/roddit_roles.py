2a7a599c2d2363ede4e78a2d77654c9ea34e5825
gc-plp <plp.github@gmail.com>
2019-09-07 10:44:39 +0000

    More role commands for roddit

--- plugins/roddit_roles.py
+++ plugins/roddit_roles.py
@@ -1,14 +1,14 @@
-from plugins.discord_utils import roles_from_list
+from plugins.discord_utils import add_role_from_list, remove_role_from_list, roles_from_list, remove_given_role_from_list
 from spanky.plugin import hook
 
 RODDIT_ID = "287285563118190592"
 
 @hook.command(server_id=RODDIT_ID)
-def color(send_message, server, event, bot, text):
+def vreau_culoare(send_message, server, event, text, bot):
     return roles_from_list(
-            "- START Culori -",
-            "- END Culori -",
-            "nu vreau culoare",
+            "----- START Culori -----",
+            "------- END Culori -------",
+            None,
             send_message,
             server,
             event,
@@ -16,25 +16,50 @@ def color(send_message, server, event, bot, text):
             text)
 
 @hook.command(server_id=RODDIT_ID)
-def joc(send_message, server, event, bot, text):
-    return roles_from_list(
-            "- START Jocuri -",
-            "- END Jocuri -",
-            "nu vreau joc",
-            send_message,
+def nu_vreau_culoare(send_message, server, event, text):
+    return remove_role_from_list(
+            "----- START Culori -----",
+            "------- END Culori -------",
             server,
             event,
-            bot,
+            send_message)
+
+@hook.command(server_id=RODDIT_ID)
+def vreau_joc(send_message, server, event, text):
+    return add_role_from_list(
+            "---- START Jocuri ----",
+            "---- END Jocuri ----",
+            server,
+            event,
+            send_message,
             text)
 
 @hook.command(server_id=RODDIT_ID)
-def rol(send_message, server, event, bot, text):
-    return roles_from_list(
-            "- START Grupuri -",
-            "- END Grupuri -",
-            "nu vreau",
-            send_message,
+def nu_vreau_joc(send_message, server, event, text):
+    return remove_given_role_from_list(
+            "---- START Jocuri ----",
+            "---- END Jocuri ----",
             server,
             event,
-            bot,
+            send_message,
+            text)
+
+@hook.command(server_id=RODDIT_ID)
+def vreau_rol(send_message, server, event, bot, text):
+    return add_role_from_list(
+            "---- START Grupuri ----",
+            "---- END Grupuri ----",
+            server,
+            event,
+            send_message,
+            text)
+
+@hook.command(server_id=RODDIT_ID)
+def nu_vreau_rol(send_message, server, event, bot, text):
+    return remove_given_role_from_list(
+            "---- START Grupuri ----",
+            "---- END Grupuri ----",
+            server,
+            event,
+            send_message,
             text)
