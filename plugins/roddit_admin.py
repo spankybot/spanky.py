28f8a42bd7d99e92f8b102abed590ea4780b24a2
gc-plp <plp.github@gmail.com>
2019-09-27 15:58:33 +0000

    Fix roddit admin server referencing

--- plugins/roddit_admin.py
+++ plugins/roddit_admin.py
@@ -9,8 +9,8 @@ from plugins.discord_utils import *
 RODDIT_ID = "287285563118190592"
 
 @hook.command
-async def votat(author, event):
-    role = get_role_by_name(roddit, "A votat")
+async def votat(author, event, server):
+    role = get_role_by_name(server, "A votat")
     author.add_role(role)
 
     try:
@@ -20,18 +20,22 @@ async def votat(author, event):
         traceback.print_exc()
 
 @hook.command(permissions=Permission.admin, server_id=RODDIT_ID)
-async def ok(event, text, str_to_id):
-    role = get_role_by_name(roddit, "Valoare")
-    user = get_user_by_id(roddit, str_to_id(text))
+async def ok(event, text, str_to_id, server):
+    try:
+        role = get_role_by_name(server, "Valoare")
+        user = get_user_by_id(server, str_to_id(text))
 
-    await event.msg.async_add_reaction(u"👍")
+        await event.msg.async_add_reaction(u"👍")
 
-    user.add_role(role)
+        user.add_role(role)
+    except:
+        import traceback
+        traceback.print_exc()
 
 @hook.command(permissions=Permission.admin, server_id=RODDIT_ID)
-def list_noobs(reply):
+def list_noobs(reply, server):
     noobs = []
-    users = roddit.get_users()
+    users = server.get_users()
 
     for user in users:
         if len(user.roles) == 0:
@@ -48,8 +52,8 @@ def list_noobs(reply):
     reply(msg)
 
 @hook.command(permissions=Permission.admin, server_id=RODDIT_ID)
-def kick_noobs(reply):
-    users = roddit.get_users()
+def kick_noobs(reply, server):
+    users = server.get_users()
 
     for user in users:
         if len(user.roles) == 0:
