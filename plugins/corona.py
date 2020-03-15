9b62850fa48304918b41321a70fad6b7ec4e0364
Vasiluță Mihai-Alexandru <alexv@siluta.ro>
2020-03-15 19:03:43 +0200

    Add .corona country codes (#15)
    * change .corona
    
    * add .corona country codes

--- plugins/corona.py
+++ plugins/corona.py
@@ -3,6 +3,62 @@ import requests
 
 base = "https://corona.lmao.ninja/"
 
+country_codes = {
+    'ae': 'uae',
+    'al': 'albania',
+    'at': 'austria',
+    'au': 'australia',
+    'ba': 'bosnia and herzegovina',
+    'be': 'belgium',
+    'bg': 'bulgaria',
+    'br': 'brazil',
+    'bw': 'botswana',
+    'by': 'belarus',
+    'ca': 'canada',
+    'ch': 'switzerland',
+    'cn': 'china',
+    'cy': 'cyprus',
+    'cz': 'czechia',
+    'de': 'germany',
+    'dk': 'denmark',
+    'dp': 'diamond princess',
+    'ee': 'estonia',
+    'eg': 'egypt',
+    'es': 'spain',
+    'fi': 'finland',
+    'fr': 'france',
+    'gb': 'uk',
+    'ge': 'georgia',
+    'gr': 'greece',
+    'hk': 'hong kong',
+    'hu': 'hungary',
+    'ie': 'ireland',
+    'in': 'india',
+    'iq': 'iraq',
+    'ir': 'iran',
+    'is': 'iceland',
+    'it': 'italy',
+    'jp': 'japan',
+    'kr': 's. korea',
+    'md': 'moldova',
+    'nl': 'netherlands',
+    'no': 'norway',
+    'nz': 'new zealand',
+    'pe': 'peru',
+    'ph': 'philippines',
+    'pk': 'pakistan',
+    'pl': 'poland',
+    'pt': 'portugal',
+    'ro': 'romania',
+    'ru': 'russia',
+    'sa': 'saudi arabia',
+    'se': 'sweden',
+    'si': 'slovenia',
+    'sk': 'slovakia',
+    'th': 'thailand',
+    'vatican': 'vatican city',
+}
+
 
 def handle_countries():
     data = requests.get(base + "countries").json()
@@ -14,29 +70,28 @@ def handle_countries():
 
 @hook.command()
 def corona(text, reply):
-    """<option> [country] - available options: [all, country]. If option is country, you must specify a country from <https://worldometers.info/coronavirus#countries>. """
+    """<option> - available options: [all, <country name>]. If option is <country name>, you must specify a country from <https://worldometers.info/coronavirus#countries>. """
 
     args = text.split(" ")
     if len(args) == 0:
-        reply("Usage: `.corona <option> [countries]`")
+        reply("Usage: `.corona <option>`")
         return
 
     if args[0] == "all" or args[0] == "total":
         response = requests.get(base + "all").json()
         reply(
             f"""`Cases: {response['cases']} | Recovered: {response['recovered']}| Deaths: {response['deaths']}`""")
+        return
 
-    elif args[0] == "country" or args[0] == "tara":
-        if len(args) != 2:
-            reply(f"usage: `.corona {args[0]} [countries]`")
-            return
+    countries = handle_countries()
 
-        countries = handle_countries()
-        country_name = args[1]
-        if country_name.lower() not in countries:
-            reply(
-                "Country not found. Check out <https://worldometers.info/coronavirus#countries> for a complete list of countries.")
-            return
-        country_data = countries[country_name.lower()]
+    if text.lower() in country_codes:
+        text = country_codes[text.lower()]
 
-        reply(f"""`Cases: {country_data['cases']} (+{country_data['todayCases']} today) | Recovered: {country_data['recovered']} | Critical: {country_data['critical']} | Deaths: {country_data['deaths']} (+{country_data['todayDeaths']} today) `""")
+    if text.lower() not in countries:
+        reply(
+            "Country not found. Check out <https://worldometers.info/coronavirus#countries> for a complete list of countries.")
+        return
+    country_data = countries[text.lower()]
+
+    reply(f"""`Cases: {country_data['cases']} (+{country_data['todayCases']} today) | Recovered: {country_data['recovered']} | Critical: {country_data['critical']} | Deaths: {country_data['deaths']} (+{country_data['todayDeaths']} today) `""")
