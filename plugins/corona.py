e9fd66fa183e77744e08e1a2d5b509fcc72fe65c
Vasiluță Mihai-Alexandru <alexv@siluta.ro>
2020-03-15 19:01:10 +0200

    add .corona country codes

--- plugins/corona.py
+++ plugins/corona.py
@@ -3,6 +3,62 @@ import requests
 
 base = "https://corona.lmao.ninja/"
 
+country_codes = {
+    'ae': 'uae',
+    'al': 'albania',
+    'at': 'austria',
+    'au': 'australia',
+    'ba': 'bosnia and herzegovina',
+    'be': 'belgium',
+    'bg': 'bulgaria',
+    'br': 'brazil',
+    'bw': 'botswana',
+    'by': 'belarus',
+    'ca': 'canada',
+    'ch': 'switzerland',
+    'cn': 'china',
+    'cy': 'cyprus',
+    'cz': 'czechia',
+    'de': 'germany',
+    'dk': 'denmark',
+    'dp': 'diamond princess',
+    'ee': 'estonia',
+    'eg': 'egypt',
+    'es': 'spain',
+    'fi': 'finland',
+    'fr': 'france',
+    'gb': 'uk',
+    'ge': 'georgia',
+    'gr': 'greece',
+    'hk': 'hong kong',
+    'hu': 'hungary',
+    'ie': 'ireland',
+    'in': 'india',
+    'iq': 'iraq',
+    'ir': 'iran',
+    'is': 'iceland',
+    'it': 'italy',
+    'jp': 'japan',
+    'kr': 's. korea',
+    'md': 'moldova',
+    'nl': 'netherlands',
+    'no': 'norway',
+    'nz': 'new zealand',
+    'pe': 'peru',
+    'ph': 'philippines',
+    'pk': 'pakistan',
+    'pl': 'poland',
+    'pt': 'portugal',
+    'ro': 'romania',
+    'ru': 'russia',
+    'sa': 'saudi arabia',
+    'se': 'sweden',
+    'si': 'slovenia',
+    'sk': 'slovakia',
+    'th': 'thailand',
+    'vatican': 'vatican city',
+}
+
 
 def handle_countries():
     data = requests.get(base + "countries").json()
@@ -28,6 +84,9 @@ def corona(text, reply):
         return
 
     countries = handle_countries()
+
+    if text.lower() in country_codes:
+        text = country_codes[text.lower()]
 
     if text.lower() not in countries:
         reply(
