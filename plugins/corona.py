68423ab959151fddfc47295d0e0a84a2d1f52c6c
Vasiluță Mihai-Alexandru <alexv@siluta.ro>
2020-03-22 19:36:29 +0200

    Add .corona_format

--- plugins/corona.py
+++ plugins/corona.py
@@ -56,6 +56,7 @@ country_codes = {
     'si': 'slovenia',
     'sk': 'slovakia',
     'th': 'thailand',
+    'us': 'usa',
     'vatican': 'vatican city',
 }
 
@@ -68,8 +69,14 @@ def handle_countries():
     return ret
 
 
+def getFormat(author, storage):
+    if author.id in storage:
+        return storage[author.id]
+    return "`Cases | Recovered | Critical | Deaths | Active | Information updated multiple times a day`"
+
+
 @hook.command()
-def corona(text, reply):
+def corona(text, reply, storage, author):
     """<option> - available options: [all, <country name>]. If option is <country name>, you must specify a country from <https://worldometers.info/coronavirus#countries>. """
 
     args = text.split(" ")
@@ -77,10 +84,10 @@ def corona(text, reply):
         reply("Usage: `.corona <option>`")
         return
 
-    if args[0] == "all" or args[0] == "total":
+    if args[0] == "all" or args[0] == "total" or text == "":
         response = requests.get(base + "all").json()
         reply(
-            f"""`Cases: {response['cases']} | Recovered: {response['recovered']}| Deaths: {response['deaths']} | Information updated multiple times a day.`""")
+            f"""`Cases: {response['cases']} | Recovered: {response['recovered']} | Deaths: {response['deaths']} | Information updated multiple times a day.`""")
         return
 
     countries = handle_countries()
@@ -89,9 +96,40 @@ def corona(text, reply):
         text = country_codes[text.lower()]
 
     if text.lower() not in countries:
-        reply(
-            "Country not found. Either the command is dead, or you need to check out <https://worldometers.info/coronavirus#countries> for a complete list of countries.")
+        reply("Country not found. Either the command is dead, or you need to check out <https://worldometers.info/coronavirus#countries> for a complete list of countries.")
         return
     country_data = countries[text.lower()]
 
-    reply(f"""`Cases: {country_data['cases']} (+{country_data['todayCases']} today) | Recovered: {country_data['recovered']} | Critical: {country_data['critical']} | Deaths: {country_data['deaths']} (+{country_data['todayDeaths']} today) | Information updated multiple times a day.`""")
+    userFormat = getFormat(author, storage)
+
+    data = {
+        "Cases": f"Cases: {country_data['cases']} (+{country_data['todayCases']} today)",
+        "Recovered": f"Recovered: {country_data['recovered']}",
+        "Critical": f"Critical: {country_data['critical']}",
+        "Deaths": f"Deaths: {country_data['deaths']} (+{country_data['todayDeaths']} today)",
+        "Active": f"Active: {country_data['active']}",
+        "C/M": f"Cases/1M: {country_data['casesPerOneMillion']}"
+    }
+
+    for key, value in data.items():
+        print(key, value)
+        userFormat = userFormat.replace(key, value, 1)
+    reply(f"{userFormat}")
+
+
+@hook.command()
+def corona_format(text, reply, storage, author):
+    """<format> - formats the .corona command for you. Every keyword in ["Cases", "Recovered", "Critical", "Deaths", "Active", "C/M"] will be replaced with the appropriate data.
+    Use `clear` if you want to clear your format and use the default one"""
+    if text == "help":
+        reply("Usage: `.corona_format <format>`\nMore information in the `.help` section for the command")
+        return
+    if text == "":
+        reply(f"Your format: {getFormat(author, storage)}")
+        return
+    if text == "clear":
+        storage.pop(author.id, None)
+        reply("Format cleared!")
+        return
+    storage[author.id] = text
+    reply("Format set!")
