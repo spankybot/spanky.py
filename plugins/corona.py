619c5ce5db5c53e719338548d3e91970ea01c33d
Vasiluță Mihai-Alexandru <alexv@siluta.ro>
2020-04-19 18:04:55 +0300

    Update .corona

--- plugins/corona.py
+++ plugins/corona.py
@@ -1,78 +1,14 @@
 from spanky.plugin import hook
 import requests
+from datetime import datetime
 
 base = "https://corona.lmao.ninja/v2/"
-
-country_codes = {
-    'ae': 'uae',
-    'al': 'albania',
-    'at': 'austria',
-    'au': 'australia',
-    'ba': 'bosnia and herzegovina',
-    'be': 'belgium',
-    'bg': 'bulgaria',
-    'br': 'brazil',
-    'bw': 'botswana',
-    'by': 'belarus',
-    'ca': 'canada',
-    'ch': 'switzerland',
-    'cn': 'china',
-    'cy': 'cyprus',
-    'cz': 'czechia',
-    'de': 'germany',
-    'dk': 'denmark',
-    'dp': 'diamond princess',
-    'ee': 'estonia',
-    'eg': 'egypt',
-    'es': 'spain',
-    'fi': 'finland',
-    'fr': 'france',
-    'gb': 'uk',
-    'ge': 'georgia',
-    'gr': 'greece',
-    'hk': 'hong kong',
-    'hu': 'hungary',
-    'ie': 'ireland',
-    'in': 'india',
-    'iq': 'iraq',
-    'ir': 'iran',
-    'is': 'iceland',
-    'it': 'italy',
-    'jp': 'japan',
-    'kr': 's. korea',
-    'md': 'moldova',
-    'nl': 'netherlands',
-    'no': 'norway',
-    'nz': 'new zealand',
-    'pe': 'peru',
-    'ph': 'philippines',
-    'pk': 'pakistan',
-    'pl': 'poland',
-    'pt': 'portugal',
-    'ro': 'romania',
-    'ru': 'russia',
-    'sa': 'saudi arabia',
-    'se': 'sweden',
-    'si': 'slovenia',
-    'sk': 'slovakia',
-    'th': 'thailand',
-    'us': 'usa',
-    'vatican': 'vatican city',
-}
-
-
-def handle_countries():
-    data = requests.get(base + "countries").json()
-    ret = dict()
-    for country in data:
-        ret[country["country"].lower()] = country
-    return ret
 
 
 def getFormat(author, storage):
     if author.id in storage:
         return storage[author.id]
-    return "`Cases | Recovered | Critical | Deaths | Active | Information updated multiple times a day`"
+    return "`Country | Cases | Recovered | Critical | Deaths | Active | Tests | Last updated at LUpdated`"
 
 
 @hook.command()
@@ -85,51 +21,57 @@ def corona(text, reply, storage, author):
         return
 
     if args[0] == "all" or args[0] == "total" or text == "":
-        response = requests.get(base + "all").json()
-        reply(
-            f"""`Cases: {response['cases']} | Recovered: {response['recovered']} | Deaths: {response['deaths']} | Information updated multiple times a day.`""")
+        country_data = requests.get(base + "all").json()
+    else:
+        country_data = requests.get(base + "countries/"+text).json()
+
+    # if the return data has a message field, it means something occured and we should print it
+    if "message" in country_data:
+        reply(country_data["message"])
         return
 
-    countries = handle_countries()
-
-    if text.lower() in country_codes:
-        text = country_codes[text.lower()]
-
-    if text.lower() not in countries:
-        reply("Country not found. Either the command is dead, or you need to check out <https://worldometers.info/coronavirus#countries> for a complete list of countries.")
-        return
-    country_data = countries[text.lower()]
+    if "country" not in country_data:
+        country_data["country"] = "World"
 
     userFormat = getFormat(author, storage)
-
     data = {
-        "Cases": f"Cases: {country_data['cases']} (+{country_data['todayCases']} today)",
-        "Recovered": f"Recovered: {country_data['recovered']}",
-        "Critical": f"Critical: {country_data['critical']}",
-        "Deaths": f"Deaths: {country_data['deaths']} (+{country_data['todayDeaths']} today)",
-        "Active": f"Active: {country_data['active']}",
-        "C/M": f"Cases/1M: {country_data['casesPerOneMillion']}"
+        "Cases": f"Cases: {country_data['cases']:,} (CToday)",
+        "Deaths": f"Deaths: {country_data['deaths']:,} (DToday)",
+        "Tests": f"Tests: {country_data['tests']:,}",
+        "CToday": f"+{country_data['todayCases']:,} today",
+        "DToday": f"+{country_data['todayDeaths']:,} today",
+        "C/M": f"Cases/1M: {country_data['casesPerOneMillion']:,}",
+        "D/M": f"Deaths/1M: {country_data['deathsPerOneMillion']:,}",
+        "T/M": f"Tests/1M: {country_data['testsPerOneMillion']:,}",
+        "Recovered": f"Recovered: {country_data['recovered']:,}",
+        "Active": f"Active: {country_data['active']:,}",
+        "Critical": f"Critical: {country_data['critical']:,}",
+        "Country": country_data['country'],
+        "Continent": country_data['continent'],
+        "LUpdated": datetime.utcfromtimestamp(country_data['updated'] // 1000).strftime('%Y-%m-%d at %H:%M:%S UTC'),
     }
 
     for key, value in data.items():
         print(key, value)
-        userFormat = userFormat.replace(key, value, 1)
+        userFormat = userFormat.replace(key, value)
     reply(f"{userFormat}")
 
 
 @hook.command()
 def corona_format(text, reply, storage, author):
-    """<format> - formats the .corona command for you. Every keyword in ["Cases", "Recovered", "Critical", "Deaths", "Active", "C/M"] will be replaced with the appropriate data.
-    Use `clear` if you want to clear your format and use the default one"""
+    """<format> - formats the .corona command for you. Every keyword in ['Cases', 'Deaths', 'Tests', 'CToday', 'DToday', 'C/M', 'D/M', 'T/M', 'Recovered', 'Active', 'Critical', 'Country', 'Continent', 'LUpdated'] will be replaced with the appropriate data. Use `clear` if you want to clear your format and use the default one"""
     if text == "help":
-        reply("Usage: `.corona_format <format>`\nMore information in the `.help` section for the command")
+        reply(corona_format.__doc__)
         return
     if text == "":
         reply(f"Your format: {getFormat(author, storage)}")
         return
     if text == "clear":
-        storage.pop(author.id, None)
-        reply("Format cleared!")
+        if author.id in storage:
+            storage.pop(author.id, None)
+            reply("Format cleared!")
+        else:
+            reply("You don't have a custom format set")
         return
     storage[author.id] = text
     reply("Format set!")
