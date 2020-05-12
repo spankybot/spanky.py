9b0aa708b3d440bbb27017dd888392df9ecc0576
gc-plp <plp.github@gmail.com>
2020-05-12 22:47:24 +0000

    Update robac roles

--- plugins/robac_roles.py
+++ plugins/robac_roles.py
@@ -6,8 +6,8 @@ ROBAC_ID = "456496203040030721"
 @hook.command(server_id=ROBAC_ID)
 def tara(send_message, server, event, bot, text):
     return roles_from_list(
-            "[Countries]",
-            "<Countries/>",
+            "-----[Countries]-----",
+            "-----<Countries/>-----",
             "nu vreau",
             send_message,
             server,
@@ -18,8 +18,8 @@ def tara(send_message, server, event, bot, text):
 @hook.command(server_id=ROBAC_ID)
 def am_luat(send_message, server, event, bot, text):
     return roles_from_list(
-            "[Bac]",
-            "<Bac/>",
+            "-----[Bac]-----",
+            "-----<Bac/>-----",
             "nu vreau",
             send_message,
             server,
