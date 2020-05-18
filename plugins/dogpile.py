9f4c9af40cbd6418c830eabaf85d635cbb87bc0a
gc-plp <plp.github@gmail.com>
2020-05-18 21:24:02 +0000

    Use google images instead of dogpile

--- plugins/dogpile.py
+++ plugins/dogpile.py
@@ -1,79 +1,100 @@
-import os
-import random
-
-import requests
-from bs4 import BeautifulSoup
-
 from spanky.plugin import hook
+from collections import deque
+from googleapiclient.discovery import build
+from spanky.utils import discord_utils as dutils
+from spanky.plugin.event import EventType
 
-search_url = "https://www.dogpile.com/search"
+LARROW=u'\U0001F448'
+RARROW=u'\U0001F449'
+dev_key = None
+dev_cx = None
+search_results = deque(maxlen=50)
 
-CERT_PATH = 'dogpile.crt'
-HEADERS = {
-        "User-Agent": "Mozilla/5.0 (X11; Linux i586; rv:31.0) Gecko/20100101 Firefox/73.0"
-}
+class GISResult():
+    def __init__(self, urls, async_send_message, search_term, event):
+        print(urls)
+        self.urls = urls
+        self.async_send_message = async_send_message
+        self.crt_page = 0
+        self.msg = None
 
-session = requests.Session()
+        self.search_term = search_term
+        self.footer = "Search author: %s" % event.author.name
 
+        search_results.append(self)
 
-@hook.on_start
-def check_certs(bot):
-    try:
-        with requests.get(search_url):
-            pass
-    except requests.exceptions.SSLError:
-        session.verify = os.path.join(str(bot.data_dir), CERT_PATH)
-    except:
-        print("error on initialize")
-    else:
-        session.verify = None
+    async def send_msg(self):
+        self.embed = dutils.prepare_embed(
+            title="Image search",
+            description="Query: %s (result %d/%d)" % (self.search_term, self.crt_page + 1, len(self.urls)),
+            image_url=self.urls[self.crt_page],
+            footer_txt=self.footer)
 
+        new_message = self.msg
+        self.msg = await self.async_send_message(embed=self.embed)
 
-def query(endpoint, text):
-    params = {'q': " ".join(text.split())}
-    with requests.get(
-            search_url + "/" + endpoint, params=params, headers=HEADERS,
-            verify=session.verify
-    ) as r:
-        r.raise_for_status()
-        return BeautifulSoup(r.content)
+        if new_message is None:
+            await self.msg.async_add_reaction(LARROW)
+            await self.msg.async_add_reaction(RARROW)
 
+    async def handle_emoji(self, event):
+        # Check if arrow left or right
+        if event.reaction.emoji.name == LARROW:
+            self.crt_page -= 1
+        elif event.reaction.emoji.name == RARROW:
+            self.crt_page += 1
+
+        # Check bounds
+        if self.crt_page >= len(self.urls):
+            self.crt_page = 0
+        elif self.crt_page < 0:
+            self.crt_page = len(self.urls) - 1
+
+        await self.send_msg()
+
+@hook.on_start()
+def load_key(bot):
+    global dev_key
+    global dev_cx
+
+    dev_key = bot.config.get("api_keys", {}).get("google_dev_key", None)
+    dev_cx = bot.config.get("api_keys", {}).get("google_cx", None)
 
 @hook.command()
-def gis(text, send_embed, event):
-    """<query> - Uses the dogpile search engine to search for images."""
-    try:
-        soup = query('images', text)
-    except:
-        return "Search engine said nope"
+async def gis(text, async_send_message, event):
+    """<query> - Search for a image."""
+    service = build("customsearch", "v1", developerKey=dev_key)
 
-    results_container = soup.find('div', {'class': 'images-bing__list'})
-    if not results_container:
-        return "No results found."
+    res = service.cse().list(
+        q=text,
+        safe="active",
+        cx=dev_cx,
+        ).execute()
 
-    results_list = results_container.find_all('div', {'class': 'image'})
-    if not results_list:
-        return "No results found."
+    urls = []
+    for img in res.get("items", []):
+        if "cse_image" in img["pagemap"].keys():
+            urls.append(img["pagemap"]["cse_image"][0]["src"])
 
-    image = random.choice(results_list)
-    image_url = image.find('a', {'class': 'link'})['href']
+    if len(urls) == 0:
+        return "No results found"
 
-    send_embed(
-        title="Image search",
-        description="Searched for: %s" % text,
-        image_url=image_url,
-        footer_txt="Search author: %s" % event.author.name)
+    await GISResult(urls, async_send_message, text, event).send_msg()
 
+@hook.event(EventType.reaction_add)
+async def parse_react(bot, event):
+    # Check if the reaction was made on a message that contains a search result
+    found = None
+    for res in search_results:
+        if res.msg.id == event.msg.id:
+            found = res
+            break
 
-@hook.command()
-def g(text):
-    """<query> - Uses the dogpile search engine to find shit on the web."""
-    soup = query('web', text)
-    results = soup.find_all('div', {'class': 'web-bing__result'})
-    if not results:
-        return "No results found."
+    if not found:
+        return
 
-    result = results[0]
-    result_url = result.find('span', {'class': 'web-bing__url'}).text
-    result_description = result.find('span', {'class': 'web-bing__description'}).text
-    return "{} -- {}".format(result_url, result_description)
+    # Handle the event
+    await found.handle_emoji(event)
+
+    # Remove the reaction
+    await event.msg.async_remove_reaction(event.reaction.emoji.name, event.author)
