af0a2d6a9658322771a6490b3ac58e2f3e1db38a
gc-plp <plp.github@gmail.com>
2020-10-10 13:03:40 +0000

    add roddit 3 word story

---
+++ plugins/custom/roddit_3ws.py
@@ -0,0 +1,12 @@
+from spanky.plugin import hook
+from spanky.plugin.permissions import Permission
+
+from spanky.plugin.event import EventType
+
+@hook.event(EventType.message)
+async def alog(event):
+    if event.channel.id != "728013984430555156":
+        return
+
+    if len(event.msg.text.split()) != 3:
+        await event.msg.delete_message()
