cd8b21dca78980536902c9b8507c6c34be62915b
gc-plp <plp.github@gmail.com>
2020-10-10 13:02:55 +0000

    Add vreau_joc command

--- plugins/custom/roddit_roles.py
+++ plugins/custom/roddit_roles.py
@@ -15,6 +15,7 @@ async def vreau_culoare(event):
         max_selectable=1)
     await sel.do_send(event)
 
+
 @hook.command(server_id=RODDIT_ID)
 async def vreau_rol(event):
     sel = carousel.RoleSelectorInterval(
@@ -23,6 +24,19 @@ async def vreau_rol(event):
         title="r/Romania roles",
         first_role="---- START Grupuri ----",
         last_role="---- END Grupuri ----",
+        max_selectable=5)
+
+    await sel.do_send(event)
+
+
+@hook.command(server_id=RODDIT_ID)
+async def vreau_joc(event):
+    sel = carousel.RoleSelectorInterval(
+        server=event.server,
+        channel=event.channel,
+        title="r/Romania roles",
+        first_role="---- START Jocuri ----",
+        last_role="---- END Jocuri ----",
         max_selectable=5)
 
     await sel.do_send(event)
