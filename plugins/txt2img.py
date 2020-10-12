e1d3436bbfc57c869273734a03ec119102e4fd60
disobedians <desenezcurcubee@gmail.com>
2020-10-13 02:24:46 +0300

    txt2img

--- plugins/txt2img.py
+++ plugins/txt2img.py
@@ -15,28 +15,32 @@ PIXEL_ON = 0  # PIL color to use for "on"
 PIXEL_OFF = 255  # PIL color to use for "off"
 
 
-
 @hook.command()
-def text_image(string, font_path=None,font_size=30,font_color=None,font_bg=None):
+def text_image(
+        string,
+        font_path=None,
+        font_size=30,
+        font_color=None,
+        font_bg=None):
     grayscale = 'RGB'
     lines = string
-    font_color=font_color or "white"
+    font_color = font_color or "white"
     large_font = font_size or 30  #
-    font_path = font_path or 'fonts/sofia.ttf'
-    font_bg=font_bg or (54, 57, 63)
+    font_path = font_path or 'plugin_data/fonts/sofia.ttf'
+    font_bg = font_bg or (54, 57, 63)
     try:
         font = PIL.ImageFont.truetype(font_path, size=large_font)
     except IOError:
         font = PIL.ImageFont.load_default()
         print('Could not use chosen font. Using default.')
-   
-    pt2px = lambda pt: int(round(pt * 46.0 / 45))  
+
+    def pt2px(pt): return int(round(pt * 46.0 / 45))
     max_width_line = max(lines, key=lambda s: font.getsize(s)[0])
     test_string = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
     max_height = pt2px(font.getsize(test_string)[1])
     max_width = pt2px(font.getsize(max_width_line)[0])
-    height = max_height * len(lines) + 40  
-    width = int(round(max_width + 120))  
+    height = max_height * len(lines) + 40
+    width = int(round(max_width + 120))
     image = PIL.Image.new(grayscale, (width, height), color=font_bg)
     draw = PIL.ImageDraw.Draw(image)
     vertical_position = 25
@@ -44,14 +48,15 @@ def text_image(string, font_path=None,font_size=30,font_color=None,font_bg=None)
     line_spacing = int(round(max_height * 0.9))  # reduced spacing seems better
     for line in lines:
         draw.text((horizontal_position, vertical_position),
-                  line, fill= font_color, font=font)
+                  line, fill=font_color, font=font)
         vertical_position += line_spacing
     c_box = PIL.ImageOps.invert(image).getbbox()
     image = image.crop(c_box)
     return image
 
+
 @hook.command()
-def txt2img(text,event,send_file,reply):
+def txt2img(text, event, send_file, reply):
     """
     Generate a image using sent text
     "<option> - available input for custom fonts:
@@ -68,30 +73,40 @@ def txt2img(text,event,send_file,reply):
     Example usage of this command :
     .text2img sofia-30-red-yellow the quick brown fox jumps over the lazy dog
     """
-    VALID_FONTS=['sofia','ostirch','diso','learning','hotel','symbols','plp','diso','default']
-    strd=str.splitlines(text)
-    parameters=strd[0].split(maxsplit=1)
+    VALID_FONTS = [
+        'sofia',
+        'ostirch',
+        'diso',
+        'learning',
+        'hotel',
+        'symbols',
+        'plp',
+        'diso',
+        'default']
+    strd = str.splitlines(text)
+    parameters = strd[0].split(maxsplit=1)
     param = parameters[0].split('-')
-    font='symbols'
-    for i in range(len(VALID_FONTS)):
-        if param[0] == VALID_FONTS[i]:
-            font=VALID_FONTS[i]
-            strd[0]=strd[0].split(' ', 1)[1] # remove param
-            break
+    font = 'symbols'
+    if(len(param) > 0 and len(strd) > 0):
+        for i in range(len(VALID_FONTS)):
+            if param[0] == VALID_FONTS[i]:
+                font = VALID_FONTS[i]
+                strd[0] = strd[0].split(' ', 1)[1]  # remove param
+                break
     font_size = 30
-    if(len(param) > 1 and int(param[1])>1 and int(param[1]) < 200):
+    if(len(param) > 1 and int(param[1]) > 1 and int(param[1]) < 200):
         font_size = param[1]
     font_color = "white"
-    if(len(param) > 2 ):
-        font_color = param[2]    
-    font_bg=None
+    if(len(param) > 2):
+        font_color = param[2]
+    font_bg = None
     try:
-        if(len(param) > 3 ):
+        if(len(param) > 3):
             font_bg = param[3]
     except Error:
-            reply("da")
-    font+='.ttf'
-    font = 'fonts/' + font
-    image = text_image(strd,font,int(font_size),font_color,font_bg)
+        print("da")
+    font += '.ttf'
+    font = 'plugin_data/fonts/' + font
+    image = text_image(strd, font, int(font_size), font_color, font_bg)
     image.save('txt2img.png')
-    send_file('content.png')
+    send_file('txt2img.png')
