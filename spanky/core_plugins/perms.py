ed6497cd0cb9d169bbd41c90945274793fee4c9f
Vasiluță Mihai-Alexandru <alexv@siluta.ro>
2021-09-18 15:25:23 +0300

    run black on core_plugins

--- spanky/core_plugins/perms.py
+++ spanky/core_plugins/perms.py
@@ -5,13 +5,15 @@ hook = Hook("permission_hook", storage_name="plugins_admin.json")
 
 # Permission context creation and validation
 
+
 @hook.global_middleware(priority=0)
 def setup_perm_ctx(action: ActionCommand, hooklet: Command):
     action.context["perms"] = {"creds": []}
 
+
 @hook.global_middleware(priority=1000000)
 def finalize_perm_filter(action: ActionCommand, hooklet: Command):
-    perms = hooklet.args.get('permissions', [])
+    perms = hooklet.args.get("permissions", [])
     if not isinstance(perms, list):
         perms = [perms]
     perms = set(perms)
@@ -21,19 +23,25 @@ def finalize_perm_filter(action: ActionCommand, hooklet: Command):
         return MiddlewareResult.CONTINUE
     return MiddlewareResult.DENY
 
+
 # Permissions
+
 
 @hook.global_middleware(priority=10)
 def perm_bot_owner(action: ActionCommand, hooklet: Command):
     if action.author.id in action.bot.config.get("bot_owners", []):
         action.context["perms"]["creds"].append("bot_owner")
 
+
 @hook.global_middleware(priority=10)
 def perm_admin(action: ActionCommand, hooklet: Command):
     storage = hooklet.hook.server_storage(action.server_id)
-    if 'admin_roles' not in storage:
-        if hooklet.args.get('permissions', None) != None:
-            action.reply("Warning! Admin not set! Use .add_admin_role to set an administrator.", check_old=False)
+    if "admin_roles" not in storage:
+        if hooklet.args.get("permissions", None) != None:
+            action.reply(
+                "Warning! Admin not set! Use .add_admin_role to set an administrator.",
+                check_old=False,
+            )
     else:
         # TODO: command_owners (maybe in another middleware?)
         allowed_roles = set(storage["admin_roles"])
@@ -41,26 +49,33 @@ def perm_admin(action: ActionCommand, hooklet: Command):
         if user_roles & allowed_roles:
             action.context["perms"]["creds"].append("admin")
 
+
 @hook.command()
 def get_admin_storage(storage):
     import json
+
     return json.dumps(storage.data)
+
 
 @hook.command(permissions="admin")
 def migrate_admin_storage(storage):
     return "TODO"
 
+
 @hook.command(permissions="admin")
 def add_admin_role(storage):
     return "TODO"
+
 
 @hook.command(permissions="admin")
 def get_admin_roles(storage):
     return "TODO"
 
+
 @hook.command(permissions="admin")
 def remove_admin_role(storage):
     return "TODO"
+
 
 @hook.global_middleware(priority=1)
 def check_server_id(action: ActionCommand, hooklet: Command):
@@ -78,6 +93,5 @@ def check_server_id(action: ActionCommand, hooklet: Command):
         print(f"Unknown server_id type for hooklet {hooklet.hooklet_id}")
         good_server = False
     if not good_server:
-        print(good_server, action.server_id is server_id, action.server_id == server_id, type(action.server_id), type(server_id), action.server_id, server_id)
         return MiddlewareResult.DENY
     return MiddlewareResult.CONTINUE
--- spanky/core_plugins/pm_handler.py
+++ spanky/core_plugins/pm_handler.py
@@ -3,10 +3,11 @@ from spanky.hook2.hooklet import MiddlewareResult
 
 hook = Hook("pm_hook")
 
+
 @hook.global_middleware(priority=5)
 def handle_pm(action: ActionCommand, hooklet: Command):
-    can_pm = hooklet.args.get('can_pm', False)
-    pm_only = hooklet.args.get('pm_only', False)
+    can_pm = hooklet.args.get("can_pm", False)
+    pm_only = hooklet.args.get("pm_only", False)
     if pm_only:
         can_pm = True
     if action.is_pm and not can_pm:
