2d66c871aacede4255ff40e158bff26c9afb0dcf
gc-plp <plp.github@gmail.com>
2021-12-26 14:25:28 +0100

    add debug container script

---
+++ Dockerfile/debug.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+docker run -it --net=host --rm --dns=1.1.1.1 -v $1:/root -v $DIR/..:/botsrc gcplp/spanky:latest /bin/bash
