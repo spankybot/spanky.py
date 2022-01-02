7dd5455abb6f830d8a4528fa06da56e1b4c9bdc4
gc-plp <plp.github@gmail.com>
2022-01-02 19:47:18 +0100

    add more entry points to the docker container

---
+++ Dockerfile/files/init_env.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [[ ! -d /botsrc/.pyenv ]]; then
+    git clone https://github.com/pyenv/pyenv.git /botsrc/.pyenv
+fi
+
+export PYENV_ROOT="/botsrc/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init -)"
+eval "$(pyenv init --path)"
+
+pyenv install -s 3.10.0
+pyenv global 3.10.0
+
+cd /botsrc/
+pip3 install wheel
+pip3 install -r requirements.txt
---
+++ Dockerfile/files/run_tests.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+source /init_env.sh
+
+python3 -m pytest -rpP
