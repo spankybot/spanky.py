3fbb3cdc7cf2acd03e2dcee4f053184499f185ae
gbionescu <plp.github@gmail.com>
2024-07-08 11:59:49 +0200

    say.py: remove say_pm and add root_say
    Signed-off-by: gbionescu <plp.github@gmail.com>

--- plugins/say.py
+++ plugins/say.py
@@ -21,22 +21,17 @@ async def say(text, server, async_send_message):
     )
 
 
-@hook.command(permissions=Permission.admin)
-def say_pm(text, bot):
+@hook.command(permissions=Permission.bot_owner)
+async def root_say(text, server, async_send_message):
     """
-    <user message> - Send a message to an user.
+    <channel message> - Send a message to a channel
     """
     data = text.split(" ", maxsplit=1)
-    user_id = dutils.str_to_id(data[0])
+    channel = dutils.get_channel_by_id(server, dutils.str_to_id(data[0]))
 
-    user = None
-    for server in bot.backend.get_servers():
-        user = dutils.get_user_by_id(server, user_id)
-        if user:
-            break
+    if not channel:
+        return "Invalid channel"
 
-    if not user:
-        return "Invalid user"
-
-    user.send_pm(data[1])
-    return "Done"
+    await async_send_message(
+        text=data[1], target=channel.id, allowed_mentions=AllowedMentions.all()
+    )
